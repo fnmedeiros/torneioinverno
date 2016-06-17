dbc546b6d1db412ad88fcf17b34ffa724410cb44
Felipe Novaes Medeiros <felipenovaesmedeiros@gmail.com>
2016-06-17 14:39:03 -0300

    Add migration add_column_person_id_on_users

---
+++ database/migrations/2016_06_17_173536_add_column_person_id_on_users.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnPersonIdOnUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function ($table) {
+            $table->unsignedInteger('person_id');
+            $table->foreign('person_id')->references('id')->on('person');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+                    $table->dropForeign('users_person_id_foreign');                    
+                });
+        Schema::table('users', function ($table) {
+            $table->dropColumn('votes');
+        });
+    }
+}
